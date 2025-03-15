fc78822c70a7012253bc4e0dd889dbdaa69965a5
x-so <naktul@proton.me>
2025-03-15 14:12:13 +0100

    Update appinfo.bat

--- scripts/appinfo.bat
+++ scripts/appinfo.bat
@@ -3,11 +3,8 @@ echo ----------------------
 echo UpKeep - Version 1.0
 echo Author - x-so
 echo ----------------------
-echo All the app documentation is either in README.txt or on my github
-echo github: https://github.com/x-s0
-echo ----------------------
-echo https://ko-fi.com/
-echo bitcoin:
+echo All the app documentation is in README or on my github
+echo github: https://github.com/x-s0/UpKeep
 echo ----------------------
 echo Legal:
-echo Read LICENSE.txt
+echo Read LICENSE
